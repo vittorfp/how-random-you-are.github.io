8e5721cf9f5fa406653708fb105197ce7e9ba796
Vittor Faria <vittorfpereira@gmail.com>
2020-07-22 18:30:44 -0300

    Initial commit.

---
+++ precache-manifest.ff40d7580c53a09816360447d96edd9b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49384a864ac0db8171c412b657eb219a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ce71927ac0ecc9fcb7",
+    "url": "/static/css/main.44931462.chunk.css"
+  },
+  {
+    "revision": "806a393a7de6e6e6059b",
+    "url": "/static/js/2.ef1465c7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ef1465c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b039533b21eb2eaf5d8d",
+    "url": "/static/js/3.181d9b39.chunk.js"
+  },
+  {
+    "revision": "feb4d5e8d8e886dab2b4",
+    "url": "/static/js/4.bdb3b74b.chunk.js"
+  },
+  {
+    "revision": "e0ce71927ac0ecc9fcb7",
+    "url": "/static/js/main.cc115c75.chunk.js"
+  },
+  {
+    "revision": "e59ed2a6cef1535fdddc",
+    "url": "/static/js/runtime-main.18cc4e37.js"
+  },
+  {
+    "revision": "46818ebd4f76c4e6fe9b030dbc74f5cf",
+    "url": "/static/media/Raleway-Black.46818ebd.ttf"
+  },
+  {
+    "revision": "f49f3d2d9df5013c9bfaab7e3d39ee57",
+    "url": "/static/media/Raleway-Bold.f49f3d2d.ttf"
+  },
+  {
+    "revision": "466d154fedd98f85c9deb363ccf859a7",
+    "url": "/static/media/Raleway-Light.466d154f.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "/static/media/Raleway-Medium.bb5ae98e.ttf"
+  },
+  {
+    "revision": "5a25c50b181b07279489ce5bb6a9545c",
+    "url": "/static/media/Raleway-SemiBold.5a25c50b.ttf"
+  }
+]);
